1e3a0c564a685f93eaaa3cd8d00b0727d117fb23
Daniil Bright <pixelcmnd@icloud.com>
2026-03-31 17:55:32 +0300

    pkgs: add neovim editor

--- home/core/editors/default.nix
+++ home/core/editors/default.nix
@@ -5,5 +5,6 @@
 }: {
   imports = [
     ./helix.nix
+    ./neovim.nix
   ];
 }
--- modules/nixos/desktop/misc/default.nix
+++ modules/nixos/desktop/misc/default.nix
@@ -30,4 +30,11 @@
       ];
     };
   };
+
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    # Add any missing dynamic libraries for unpackaged programs
+    # here, NOT in environment.systemPackages
+    tree-sitter
+  ];
 }
