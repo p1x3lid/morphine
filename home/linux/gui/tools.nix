612255a92fd9664d9ffa42f8c79b31bf2c6b2974
Daniil Bright <pixelcmnd@icloud.com>
2026-04-08 14:16:06 +0300

    pkgs: add veracrypt

--- home/linux/gui/tools.nix
+++ home/linux/gui/tools.nix
@@ -12,5 +12,6 @@
       nautilus # File manager for GNOME
       appflowy # Notion Alternative
       ungoogled-chromium # Open source web browser from Google, with dependencies on Google web services removed
+      veracrypt # Free Open-Source filesystem on-the-fly encryption
     ]);
 }
