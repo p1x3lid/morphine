64c0421bb6f28891ff4917902a7f0670edf3aec2
Daniil Bright <pixelcmnd@icloud.com>
2026-04-14 18:10:58 +0300

    chore: remove duplicates after migrating to niri-flake

--- home/linux/gui/niri/hypr/default.nix
+++
@@ -1,9 +0,0 @@
-{pkgs, ...}: {
-  imports = [
-    ./hypridle.nix
-  ];
-
-  home.packages = with pkgs; [
-    hypridle
-  ];
-}
--- home/linux/gui/niri/hypr/hypridle.nix
+++
@@ -1,6 +0,0 @@
-{...}: {
-  # Hyprland idle daemon
-  services.hypridle.enable = true;
-
-  xdg.configFile."hypr/hypridle.conf".source = ./hypridle.conf;
-}
